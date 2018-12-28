854b38179d33c59aa0292887bb8939206c216388
liudonglin <454812428@qq.com>
2018-12-28 16:47:16 +0800

    update

---
+++ stress-test/Controllers/OOMController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace stress_test.Controllers
+{
+    [Route("stress/[controller]")]
+    [ApiController]
+    public class OOMController: ControllerBase
+    {
+        // GET stress/oom
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            string myPassword = "Test the cpu stress use BCrypt ";
+
+            for (int i = 0; i < 24; i++)
+            {
+                myPassword += myPassword + Guid.NewGuid().ToString();
+            }
+
+            var size = "not count";
+
+            var length = Encoding.UTF8.GetBytes(myPassword).Length;
+            size = length>1048576?length/1048576+"MB":length/1024+"KB";
+
+            return "Memory Total " + size;
+        }
+    }
+}
